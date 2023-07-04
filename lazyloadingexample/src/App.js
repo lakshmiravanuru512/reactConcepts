f3533667e5b7c069ef65b89993403db5c9b08bde
lakshmiravanuru512 <lakshmirevanuru512@gmail.com>
2023-07-04 11:12:47 +0530

    lazyloading example

--- lazyloadingexample/src/App.js
+++ lazyloadingexample/src/App.js
@@ -1,11 +1,29 @@
-import './App.css';
-import LazyComponent from './Components/LazyComponent';
-function App() {
+import React from "react";
+import { Link, Route, Routes } from "react-router-dom";
+import {Home} from "./Components/Home";
+// import About from "./About";
+const About = React.lazy(() => import("./Components/About"));
+
+const App = () => {
   return (
     <div className="App">
-      <LazyComponent/>
+      <div>
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+      </div>
+      <Routes>
+        <Route index element={<Home />} />
+        <Route
+          path="about"
+          element={
+            <React.Suspense fallback={<div>Loading..</div>}>
+              <About />
+            </React.Suspense>
+          }
+        />
+      </Routes>
     </div>
   );
-}
+};
 
 export default App;
---
+++ lazyloadingexample/src/Components/About.js
@@ -0,0 +1,30 @@
+import React from 'react'
+
+export const About = () => {
+  return (
+    <div>
+        <h1>About</h1>
+        <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam tincidunt
+        ullamcorper neque, vehicula egestas nisl consequat eget. Phasellus
+        volutpat ante quis sem volutpat, maximus consequat sem pulvinar. Etiam
+        iaculis felis posuere vulputate finibus. Aliquam purus velit, bibendum
+        eget tincidunt ac, vehicula et est. Mauris a velit nec magna pulvinar
+        rutrum sit amet nec lacus. Nullam ut nulla hendrerit, tincidunt dui at,
+        cursus diam. Donec vel leo purus. Donec sit amet orci sapien. Cras
+        aliquam luctus lacus. Nulla arcu augue, hendrerit vitae dolor id,
+        consequat placerat tortor. Phasellus at nisl metus. Nulla convallis,
+        turpis et porttitor dignissim, justo justo venenatis magna, a pharetra
+        tortor tortor sed felis. Sed lobortis urna eget ultricies vulputate.
+        Nunc arcu ipsum, vulputate eu arcu ac, lacinia tristique turpis.
+        Pellentesque condimentum ante in est scelerisque, at pellentesque ligula
+        aliquet. Suspendisse vitae tortor turpis. Etiam vel dolor vel mauris
+        pharetra dapibus. Nulla vehicula, lacus eget accumsan viverra, leo neque
+        feugiat magna, rhoncus luctus nunc est in ante. Donec diam dui,
+        imperdiet sit amet nisl eu, efficitur posuere massa. Aenean elementum
+        lacinia convallis. Sed ipsum nulla, faucibus id ex a, consectetur
+        gravida lacus. Nulla facilisi. Etiam pulvinar risus sit amet justo
+        tincidunt, non aliquet nunc ornare. Aliquam erat volutpat. </p>
+    </div>
+    
+  )
+}
